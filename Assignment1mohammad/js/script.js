ae66963dbbf20dc0271ba72975e5772a6d42e190
Mohammad B <127884189+Muhamm3dd@users.noreply.github.com>
2023-06-07 16:43:27 -0400

    Add files via upload

---
+++ Assignment1mohammad/js/script.js
@@ -0,0 +1,9 @@
+
+    function toggleLyrics() {
+      var lyrics = document.getElementById('lyrics');
+      if (lyrics.style.display === 'none') {
+        lyrics.style.display = 'block';
+      } else {
+        lyrics.style.display = 'none';
+      }
+    }
